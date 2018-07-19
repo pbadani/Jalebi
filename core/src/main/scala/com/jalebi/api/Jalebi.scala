a6385276a69d60467415922a4cc7913d502d1875
Parag <parag.badani@gmail.com>
2018-07-19 22:06:58 +0530

    Some implementation methods for vertex.

--- core/src/main/scala/com/jalebi/api/Jalebi.scala
+++ core/src/main/scala/com/jalebi/api/Jalebi.scala
@@ -1,9 +1,9 @@
 package com.jalebi.api
 
-case class Jalebi[V, E] {
+case class Jalebi[V, E] private(vertices: Seq[Vertex[V]], edges: Seq[Edge[E]]) {
 
 }
 
 object Jalebi {
- def apply (vertices: Set[V], )
+  def apply[V, E](vertices: Seq[Vertex[V]], edges: Seq[Edge[E]]): Jalebi[V, E] = Jalebi(vertices, edges)
 }
--- core/src/main/scala/com/jalebi/api/Vertex.scala
+++ core/src/main/scala/com/jalebi/api/Vertex.scala
@@ -1,5 +1,7 @@
 package com.jalebi.api
 
+import scala.collection.mutable
+
 trait Vertex[T] extends Serializable {
 
   protected val id: VertexID
@@ -7,6 +9,18 @@ trait Vertex[T] extends Serializable {
   protected val data: T
 
   protected val relations: Seq[Edge[T] with EdgeDirection with EdgeWeight]
+
+  private val visitedByJobIDs: mutable.Set[String] = new mutable.HashSet[String]()
+
+  def markVisitedByJob(jobID: String): Unit = {
+    require(jobID.nonEmpty)
+    visitedByJobIDs += jobID
+  }
+
+  def isVisitedByJob(jobID: String): Boolean = {
+    require(jobID.nonEmpty)
+    visitedByJobIDs.contains(jobID)
+  }
 
   def getOutgoing: Seq[Edge[T] with EdgeDirection with EdgeWeight] = {
     relations.filter(e => (e.isDirected && e.isSource(this.id))
@@ -19,7 +33,7 @@ trait Vertex[T] extends Serializable {
   }
 
   override def toString: String = {
-    s"VertexID: ${id}"
+    s"VertexID: $id"
   }
 }
 

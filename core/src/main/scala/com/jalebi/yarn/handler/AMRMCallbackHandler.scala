2d8237a942d436fe007890e57c81e43e9a4ed461
Parag <parag.badani@gmail.com>
2018-07-28 14:52:04 +0530

    Add RM and NM callback handler implementation.

--- core/src/main/scala/com/jalebi/yarn/handler/AMRMCallbackHandler.scala
+++ core/src/main/scala/com/jalebi/yarn/handler/AMRMCallbackHandler.scala
@@ -1,12 +1,15 @@
 package com.jalebi.yarn.handler
 
 import java.util
+import java.util.stream.Collectors
 
+import com.jalebi.executor.Executor
+import com.jalebi.utils.Logging
+import com.jalebi.yarn.{ApplicationMaster, ContainerStateManager}
 import org.apache.hadoop.yarn.api.records.{Container, ContainerStatus, NodeReport, UpdatedContainer}
 import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync.AbstractCallbackHandler
 
-class AMRMCallbackHandler extends AbstractCallbackHandler {
-
+class AMRMCallbackHandler(applicationMaster: ApplicationMaster, containerStateManager: ContainerStateManager) extends AbstractCallbackHandler with Logging {
 
   override def onContainersUpdated(containers: util.List[UpdatedContainer]): Unit = ???
 
@@ -14,17 +17,39 @@ class AMRMCallbackHandler extends AbstractCallbackHandler {
 
   override def onShutdownRequest(): Unit = ???
 
-  override def onContainersCompleted(statuses: util.List[ContainerStatus]): Unit = ???
+  override def onContainersCompleted(statuses: util.List[ContainerStatus]): Unit = {
+    containerStateManager.containersCompleted(statuses)
+  }
 
   override def getProgress: Float = ???
 
   override def onNodesUpdated(updatedNodes: util.List[NodeReport]): Unit = ???
 
-  override def onContainersAllocated(containers: util.List[Container]): Unit = {
 
+
+  override def onContainersAllocated(containers: util.List[Container]): Unit = {
+    LOGGER.info(s"Containers Allocated: ${containers.stream().map(container => container.getId).collect(Collectors.joining(", "))}")
+    if (containerStateManager.areAllContainerRequestsFulfilled()) {
+      applicationMaster.releaseContainers(containers)
+    } else {
+      containerStateManager.containersAllocated(containers)
+      containers.forEach(container => {
+        val executorID = Executor.newExecutorID
+
+        LOGGER.info(s"Launching executor on a new container:" +
+          s" | Jalebi Executor id: $executorID" +
+          s" | Container id: ${container.getId}" +
+          s" | Node id: ${container.getNodeId}" +
+          s" | Node address: ${container.getNodeHttpAddress}" +
+          s" | Container memory: ${container.getResource.getMemorySize}" +
+          s" | Container vcores: ${container.getResource.getVirtualCores}".stripMargin('|'))
+
+        val launchThread = createLaunchContainerThread(allocatedContainer, yarnShellId)
+      })
+    }
   }
 }
 
 object AMRMCallbackHandler {
-  def apply(): AMRMCallbackHandler = new AMRMCallbackHandler()
+  def apply(applicationMaster: ApplicationMaster, containerStateManager: ContainerStateManager): AMRMCallbackHandler = new AMRMCallbackHandler(applicationMaster, containerStateManager)
 }
--- core/src/main/scala/com/jalebi/yarn/handler/NMCallbackHandler.scala
+++ core/src/main/scala/com/jalebi/yarn/handler/NMCallbackHandler.scala
@@ -3,31 +3,68 @@ package com.jalebi.yarn.handler
 import java.nio.ByteBuffer
 import java.util
 
+import com.jalebi.utils.Logging
+import com.jalebi.yarn.{ApplicationMaster, ContainerStateManager}
 import org.apache.hadoop.yarn.api.records.{ContainerId, ContainerStatus, Resource}
 import org.apache.hadoop.yarn.client.api.async.NMClientAsync.AbstractCallbackHandler
 
-class NMCallbackHandler extends AbstractCallbackHandler{
+class NMCallbackHandler(applicationMaster: ApplicationMaster, containerStateManager: ContainerStateManager) extends AbstractCallbackHandler with Logging {
+
   override def onGetContainerStatusError(containerId: ContainerId, t: Throwable): Unit = ???
 
   override def onContainerStatusReceived(containerId: ContainerId, containerStatus: ContainerStatus): Unit = ???
 
-  override def onContainerResourceIncreased(containerId: ContainerId, resource: Resource): Unit = ???
+  override def onContainerResourceIncreased(containerId: ContainerId, resource: Resource): Unit = {
+    LOGGER.info(s"Container resource increased:" +
+      s" | Container Id: $containerId" +
+      s" | Container memory: ${resource.getMemorySize}" +
+      s" | Container vcores: ${resource.getVirtualCores}".stripMargin('|'))
+  }
 
-  override def onStopContainerError(containerId: ContainerId, t: Throwable): Unit = ???
+  override def onStopContainerError(containerId: ContainerId, t: Throwable): Unit = {
+    LOGGER.error(s"Error while stopping container:" +
+      s" | Container Id: $containerId" +
+      s" | Error: ${t.getMessage}".stripMargin('|'))
+  }
 
-  override def onContainerStopped(containerId: ContainerId): Unit = ???
+  override def onContainerStopped(containerId: ContainerId): Unit = {
+    LOGGER.info(s"Container stopped:" +
+      s" | Container Id: $containerId".stripMargin('|'))
+    containerStateManager.containerCompleted(containerId)
+  }
 
-  override def onIncreaseContainerResourceError(containerId: ContainerId, t: Throwable): Unit = ???
+  override def onIncreaseContainerResourceError(containerId: ContainerId, t: Throwable): Unit = {
+    LOGGER.error(s"Error while increasing container resource:" +
+      s" | Container Id: $containerId" +
+      s" | Error: ${t.getMessage}".stripMargin('|'))
+  }
 
-  override def onStartContainerError(containerId: ContainerId, t: Throwable): Unit = ???
+  override def onStartContainerError(containerId: ContainerId, t: Throwable): Unit = {
+    LOGGER.error(s"Error while starting container:" +
+      s" | Container Id: $containerId" +
+      s" | Error: ${t.getMessage}".stripMargin('|'))
+    //TODO
+  }
 
-  override def onUpdateContainerResourceError(containerId: ContainerId, t: Throwable): Unit = ???
+  override def onUpdateContainerResourceError(containerId: ContainerId, t: Throwable): Unit = {
+    LOGGER.error(s"Error while updating container resource:" +
+      s" | Container Id: $containerId" +
+      s" | Error: ${t.getMessage}".stripMargin('|'))
+  }
 
-  override def onContainerStarted(containerId: ContainerId, allServiceResponse: util.Map[String, ByteBuffer]): Unit = ???
+  override def onContainerStarted(containerId: ContainerId, allServiceResponse: util.Map[String, ByteBuffer]): Unit = {
+    LOGGER.info(s"Container launched $containerId")
+    containerStateManager.containerLaunched(containerId)
+  }
 
-  override def onContainerResourceUpdated(containerId: ContainerId, resource: Resource): Unit = ???
+  override def onContainerResourceUpdated(containerId: ContainerId, resource: Resource): Unit = {
+    LOGGER.info(s"Container resource updated:" +
+      s" | Container Id: $containerId" +
+      s" | Container memory: ${resource.getMemorySize}" +
+      s" | Container vcores: ${resource.getVirtualCores}".stripMargin('|'))
+  }
 }
 
 object NMCallbackHandler {
-  def apply(): NMCallbackHandler = new NMCallbackHandler()
+  def apply(applicationMaster: ApplicationMaster, containerStateManager: ContainerStateManager): NMCallbackHandler = new NMCallbackHandler(applicationMaster, containerStateManager)
 }
